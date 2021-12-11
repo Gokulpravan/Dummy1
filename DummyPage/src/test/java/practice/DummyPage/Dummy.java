515832ead60b0ad1a78c1ef8b25a437d8ab4a674
Dell <Dell@Gokul>
2021-12-11 14:57:01 +0530

    Success

---
+++ DummyPage/src/test/java/practice/DummyPage/Dummy.java
@@ -0,0 +1,11 @@
+package practice.DummyPage;
+
+public class Dummy {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+       System.out.println("Welcome");
+	}
+
+}
---
+++ DummyPage/src/test/java/practice/DummyPage/Sample.java
@@ -0,0 +1,11 @@
+package practice.DummyPage;
+
+public class Sample {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+     System.out.println("Bye bye see u later");
+	}
+
+}
